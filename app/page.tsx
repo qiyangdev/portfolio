2e8c118ae270109d7b95e87a23e177a47c562d3d
Qiyang <wangqiyangx@gmail.com>
2026-06-08 07:35:28 +0800

    Reset project.

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,3 @@
-import { BlogPosts } from 'app/components/posts'
-import { siteConfig } from 'app/site'
-
-export default function Page() {
-  return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        {siteConfig.name}
-      </h1>
-      <p className="mb-4">
-        {`I'm Qiyang, a software developer who builds web products and writes about
-        the ideas, tools, and problems I keep coming back to. This site is my
-        personal space for sharing what I'm learning and the things worth keeping.`}
-      </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
-    </section>
-  )
+export default function Home() {
+  return <div></div>;
 }
